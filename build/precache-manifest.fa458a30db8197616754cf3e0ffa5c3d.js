e0cb70f7aa7315b09c26679d3232090fbe8a43b9
madhaniyer <madhaniyer@gmail.com>
2019-07-08 20:58:10 +1200

    new build

---
+++ build/precache-manifest.fa458a30db8197616754cf3e0ffa5c3d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86d6cc14331e74eaa9b791caa57d4086",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df205a117ffb63645593",
+    "url": "/static/css/main.886844e5.chunk.css"
+  },
+  {
+    "revision": "d9f1264beababf975dd0",
+    "url": "/static/js/2.a5febc9b.chunk.js"
+  },
+  {
+    "revision": "df205a117ffb63645593",
+    "url": "/static/js/main.82cd3eae.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
